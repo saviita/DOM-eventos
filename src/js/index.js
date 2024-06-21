ce9610614fb66025cbd7e7ad84cb1d5d2db29e8e
saviita <taatt2000@gmail.com>
2024-06-21 10:18:20 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+//const buttonElement = document.getElementById('button');
+//const printClick = () => {
+//  console.log('click');
+//};
+//buttonElement.addEventListener('click', printClick);
+
+const buttonElement = document.getElementById('button');
+const printClick = event => {
+  console.log(event);
+};
+
+buttonElement.addEventListener('click', printClick);
+
+//Window es quien escucha el evento y no el body o hatml
+const viewScroll = event => {
+  console.log(event);
+};
+window.addEventListener('scroll', viewScroll);
